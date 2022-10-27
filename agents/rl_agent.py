216019f08c7888b6f01b74a82c3d0ce5caa83987
xynlophyl <nlin01@wesleyan.edu>
2022-10-27 12:41:38 -0400

    initial rl agent commit

---
+++ agents/rl_agent.py
@@ -0,0 +1,12 @@
+from soccerpy.agent import Agent as BaseAgent
+from soccerpy.world_model import WorldModel
+
+# reinforcement learning agent
+
+class Agent(BaseAgent):
+
+    def get_available_moves(self):
+        pass
+
+    def think(self):
+        pass
